25dabe436bd526f6eadaef6e0e0c319c4696326d
杜满义 <dieche1227@163.com>
2017-05-16 17:00:35 +0800

    shujukujianbiao

---
+++ database/migrations/2014_10_12_000000_create_data_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+    
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('guid');
+            $table->string('password');
+            $table->tinyint('is_admin')->default(0)->comment('是否是管理员，默认0');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_users');
+    }
+}
