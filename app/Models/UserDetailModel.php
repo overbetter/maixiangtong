7672d8335e3b60dfc490cf4d22b46eb645097fe2
杜满义 <dieche1227@163.com>
2017-05-20 08:27:32 +0800

    userDetailModel

---
+++ app/Models/UserDetailModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserDetailModel extends Model
+{
+    protected $table = 'rel_user_detail';
+    protected $dateFormat = 'U';
+    // protected $guarded = ['id'];
+}
